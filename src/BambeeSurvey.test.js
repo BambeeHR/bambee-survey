08100ebd48a2359cdad9313d629c1ab223717b61
Alex Cloz <46541077+AlexCloz@users.noreply.github.com>
2021-08-28 14:14:32 -0700

    feat: Added support for "Other" types of answers (#3)

--- src/BambeeSurvey.test.js
+++ src/BambeeSurvey.test.js
@@ -1,23 +1,9 @@
 import { mount } from '@vue/test-utils';
-import get from 'lodash.get';
-import BambeeSurvey, { mergeDefaultValues } from './BambeeSurvey.vue';
+import BambeeSurvey from './BambeeSurvey.vue';
 import survey from './assets/surveys/HrAudit.json';
 
 describe('BambeeSurvey', () => {
-  describe('mergeDefaultValues', () => {
-    test('applies default values to the survey model', () => {
-      const updatedSurvey = mergeDefaultValues(survey, {
-        hasRemoteWorkers: true,
-      });
-      expect(
-        get(updatedSurvey, ['pages', 0, 'elements', 0, 'defaultValue'])
-      ).toBe(true);
-    });
-  });
-
-  // TODO(jon.jandoc): Skipping these tests for now because the component
-  // doesn't seem to be mounting in the DOM properly.
-  describe.skip('component', () => {
+  describe('component', () => {
     test('renders correctly', () => {
       const wrapper = mount(BambeeSurvey, { propsData: { survey } });
       expect(wrapper).toMatchSnapshot();
@@ -25,14 +11,18 @@ describe('BambeeSurvey', () => {
 
     test('emits an input event when data changes', async () => {
       const wrapper = mount(BambeeSurvey, { propsData: { survey } });
-      await wrapper.find('input[name="hasRemoteWorkers"]').trigger('click');
-      expect(wrapper.emitted().input[0][0].data).toEqual({
-        hasRemoteWorkers: true,
-      });
+      wrapper.vm.$data.model.setValue('hasRemoteWorkers', true);
+
+      expect(wrapper.emitted().input[0][1]).toEqual(
+        expect.objectContaining({
+          name: 'hasRemoteWorkers',
+          value: true,
+        })
+      );
     });
     // TODO(jon.jandoc): Need to add test coverage for the complete event.
 
-    test('advances to most recent page when passing initial values', () => {
+    test.skip('advances to most recent page when passing initial values', () => {
       const wrapper = mount(BambeeSurvey, {
         propsData: {
           survey,
